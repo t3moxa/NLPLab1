cb2fea42fff039942cca94e080c5ec4af9abd922
t3moxa <133843440+t3moxa@users.noreply.github.com>
2024-10-15 15:55:43 +0700

    Init

---
+++ NLPLab1.py
@@ -0,0 +1,17 @@
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk import sent_tokenize
+import pymorphy3
+
+m = pymorphy3.MorphAnalyzer()
+f = open(r"d:\Work\4NLP\text.txt")
+string = f.read()
+
+tokens = word_tokenize(string)
+
+for index, token in enumerate(tokens):
+    w = m.parse(token)[0]
+    if ('NOUN' in w.tag) or ('ADJF' in w.tag):
+        wNext = m.parse(tokens[index+1])[0]
+        if ((('NOUN' in wNext.tag) or ('ADJF' in wNext.tag)) and (w.tag.gender == wNext.tag.gender) and (w.tag.case == wNext.tag.case) and (w.tag.number == wNext.tag.number)):
+            print(w.normal_form, wNext.normal_form)
